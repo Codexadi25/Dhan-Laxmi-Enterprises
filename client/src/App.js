9b3cb026f7ba668c5c4646b2a9d8ec771d28d987
Codexadi25 <111916672+Codexadi25@users.noreply.github.com>
2024-03-05 13:55:53 +0530

    renamed:    client/src/App.jsx -> client/src/App.js 	deleted:    client/src/Err.jsx 	deleted:    client/src/Header.css 	renamed:    client/src/Store.css -> client/src/Header.js 	deleted:    client/src/Header.jsx 	deleted:    client/src/Home.jsx 	deleted:    client/src/Store.jsx
    	modified:   client/src/App.js
    	modified:   client/src/Header.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-
-import Header from './Header.jsx';
+import Header from './Header.js';
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      
+      App
     </div>
   );
 }
