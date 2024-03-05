e34427fc7f3afe264b97b4cfeeb4d4e71afe391f
Codexadi25 <111916672+Codexadi25@users.noreply.github.com>
2024-03-05 13:04:54 +0530

    modified:   client/src/App.jsx 	new file:   client/src/Header.css 	new file:   client/src/Header.jsx 	modified:   client/src/Home.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,13 @@
 import './App.css';
-import { Router, Routes, Route } from "react-router-dom";
-import Home from './Home.jsx';
-import Err from './Err.jsx';
-import Store from './Store.jsx';
+
+import Header from './Header.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-
-
-      <Router>
-         <Routes>
-            <Route path='*' element={<Err/>}/>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/home' element={<Home/>}/>
-            <Route path='/store' element={<Store/>}/>
-         </Routes>
-      </Router>
+      <Header/>
+      
     </div>
   );
 }
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -4,16 +4,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <div className='header'>
-         <a href='/'>Home</a>
-         <a href='/'>Store</a>
-         <a href='/sell'>Join Us</a>
-         <a href='/career'>Career</a>
-         <a href='#about'>About</a>
-      </div>
-
-
-      
+            
     </div>
   )
 }
