c7e0a981cb7c16a7e8cf1b3b500ee933a864694e
Codexadi25 <111916672+Codexadi25@users.noreply.github.com>
2024-05-28 19:11:56 +0530

    new file:   Dhanlaxmi.Console.html 	new file:   server/models/listing.js 	new file:   server/package-lock.json 	new file:   server/package.json 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const express = require("express");
-const engine = require('ejs-mate')
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const path = require("path");
-const methodOverride = require("method-override");
+
 
 const dbURI = "mongodb+srv://serverBotA:2VSefUSIrk5FlRTX@cluster0.tiu5lne.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // const dbURI = "mongodb+srv://creedracer111:CBG7ewqYYP4KsGu2@cluster0.lbblfev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -22,85 +19,11 @@ async function main() {
   await mongoose.connect(dbURI);
 }
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.engine("ejs",engine);
-app.use(express.static(path.join(__dirname, "/public")));
-
 app.get("/", (req, res) => {
-  res.redirect("/external/listings");
-});
-
-
-
-//Index Route
-app.get("external/listings", async (req, res) => {
-  res.render("listings/index.ejs", );
-});
-
-//New Route
-app.get("external/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
-});
-
-//Show Route
-app.get("external/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
-
-//Create Route
-app.post("external/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
-
-//Edit Route
-app.get("external/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
-
-//Update Route
-app.put("external/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
-
-//Delete Route
-app.delete("external/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
-});
-
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "My New Villa",
-    description: "By the beach",
-    price: 1200,
-    location: "Calangute, Goa",
-    country: "India",
-  });
-
-  await sampleListing.save();
-  console.log("sample was saved");
-  res.send("successful testing");
-});
-
-// NO Such Page 
-app.get("*", (req, res) => {;
-   res.render()
-});
-
+  res.send("Server listelning well!");
+  console.log("A request recieved.")
+})
 
 app.listen(8080, () => {
-  console.log("server is listening to port 8080");
-});
+  console.log('Server Listening on PORT: 8080.');
+})
